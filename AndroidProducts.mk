b7f25cd739f8c5efac77a40c2921c56083eff636
ritvikct <ritvikct@proton.me>
2024-12-21 21:29:56 +0000

    miatoll: adapt for ArrowOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_miatoll.mk
+    $(LOCAL_DIR)/arrow_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_miatoll-user \
-    aosp_miatoll-userdebug \
-    aosp_miatoll-eng
+    arrow_miatoll-user \
+    arrow_miatoll-userdebug \
+    arrow_miatoll-eng
