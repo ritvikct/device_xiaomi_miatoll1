942ce79e458c6d4c3c20b2bc19c7f18ca26e533d
ritvikct <ritvikct@proton.me>
2024-12-23 05:16:24 +0530

    tweaks

--- device.mk
+++ device.mk
@@ -64,7 +64,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    GCamGOPrebuilt-V2
+    GCamGOPrebuilt-V2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Remove unwanted packages
@@ -464,8 +464,27 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
+
+# ART
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+DONT_DEXPREOPT_PREBUILTS := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+WITH_DEXPREOPT_DEBUG_INFO := false
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# APEX
+OVERRIDE_TARGET_FLATTEN_APEX := true
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Speed up
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    Settings \
+    SystemUI \
+    NexusLauncherRelease
